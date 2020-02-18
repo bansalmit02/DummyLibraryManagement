c888e8d45cb7130bfb58c3db22c3119d5a189dbd
Asif <asifanwar007>
2020-02-18 11:24:25 +0530

    heroku push

--- project1/Welcome.php
+++ project1/Welcome.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Database Acces</title>
+</head>
 <body>
 <!-- <h1>Welcome to Our First Page</h1>
 <p>This is our first html page</p>
 <a href="https://facebook.com"> fb link</a>
 <img src="/home/asif/Pictures/Screenshot from 2020-02-14 16-13-31.png" alt="Asif Anwar" width="400" height="300"> -->
-<h1>My first PHP page</h1>
+
 
 <?php
 echo "My first PHP script";
